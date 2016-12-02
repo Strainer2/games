74b0f1aa001e34b1758dc2066737098ad5ab2bd7
John Strainer <JBone@0587334024.wireless.umich.net>
2016-12-02 13:50:58 -0500

    friday 2

--- project4.py
+++ project4.py
@@ -1,31 +1,33 @@
 import pygame
 import random
+from pygame.sprite import *
+from pygame.locals import *
 
-# define some colors (R, G, B)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 BLUE = (0, 0, 255)
+LIGHTBLUE= (30, 144, 255)
 # game settings
 WIDTH = 480
 HEIGHT = 600
 FPS = 60
-TITLE = "SHMUP"
-BGCOLOR = BLACK
-
-############  DEFINE SPRITES  ############
-class Player(pygame.sprite.Sprite):
-    # player sprite - moves left/right, shoots
+TITLE = "PIRATES ARE COMING!!!!"
+BGCOLOR = LIGHTBLUE
+########
+class Captain(pygame.sprite.Sprite):
+    # captain sprite - moves left/right, shoots
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image=pygame.image.load("/Users/JBone/Documents/tank.png.bmp").convert_alpha()
-        #self.image = pygame.Surface((50, 40))
-        #self.image.fill(GREEN)
+        self.image=pygame.image.load("img/sssship.bmp").convert_alpha()
         self.rect = self.image.get_rect()
+        self.radius = 25
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
+        self.rect.left= WIDTH / 4
+        self.rect.right = WIDTH / 4
         self.speedx = 0
 
     def update(self):
@@ -36,27 +38,34 @@ class Player(pygame.sprite.Sprite):
             self.speedx = -5
         if keystate[pygame.K_RIGHT]:
             self.speedx = 5
-
-        # move the sprite
         self.rect.x += self.speedx
         # stop at the edges
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
         if self.rect.left < 0:
             self.rect.left = 0
-
+        #shooting bullets to kill Pirates
     def shoot(self):
         bullet = Bullet(self.rect.centerx, self.rect.top)
         all_sprites.add(bullet)
         bullets.add(bullet)
+        #shooting spears to kill Sharks and Whales
+    def power(self):
+        spear= Spear(self.rect.left, self.rect.top)
+        all_sprites.add(spear)
+        spears.add(spear)
+        #getting two spears to shoot at saem time of edges of Captains ship
+        spear2= Spear(self.rect.right, self.rect.top)
+        all_sprites.add(spear2)
+        spears.add(spear2)
 
-class Mob(pygame.sprite.Sprite):
-    # mob sprite - spawns above top and moves downward
+class Pirate(pygame.sprite.Sprite):
+    #spawns pirates above window and moves downward
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((30, 40))
-        self.image.fill(RED)
+        self.image=pygame.image.load("img/pppship.bmp").convert_alpha()
         self.rect = self.image.get_rect()
+        self.radius= int(self.rect.width /2)
         self.rect.x = random.randrange(WIDTH - self.rect.width)
         self.rect.y = random.randrange(-80, -50)
         self.speedy = random.randrange(1, 8)
@@ -69,19 +78,30 @@ class Mob(pygame.sprite.Sprite):
             self.speedy = random.randrange(1, 8)
 
 
-class Meteor(Mob):
+class Shark(Pirate):
+    #spawns Sharks
     def __init__(self):
-        Mob.__init__(self)
-        self.image=pygame.Surface((15, 20))
-        self.image.fill(BLUE)
-        self.speedy=random.randrange(5, 10)
+        Pirate.__init__(self)
+        self.image=pygame.image.load("img/ssshark.bmp").convert_alpha()
+        self.rect= self.image.get_rect()
+        self.radius= int(self.rect.width /2)
+        self.speedy=random.randrange(9, 12)
+
+class Whale(Pirate):
+    #spawns Whale
+    def __init__(self):
+        Pirate.__init__(self)
+        self.image=pygame.image.load("img/whale.bmp").convert_alpha()
+        self.rect= self.image.get_rect()
+        self.radius= int(self.rect.width /2)
+        self.speedy= random.randrange(2,5)
 
 
 class Bullet(pygame.sprite.Sprite):
+    #creates Bullet Sprite
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((10, 20))
-        self.image.fill(YELLOW)
+        self.image=pygame.image.load("img/bullet.bmp").convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.bottom = y
         self.rect.centerx = x
@@ -93,66 +113,181 @@ class Bullet(pygame.sprite.Sprite):
         if self.rect.bottom < 0:
             self.kill()
 
+class Spear(Bullet):
+    #creats Spear Sprite
+    def __init__ (self, x, y):
+        Bullet.__init__(self, x, y)
+        self.image=pygame.image.load("img/sspear.bmp").convert_alpha()
+##########################################
+class Option:
+    #creates beginning screen
+    def __init__(self, text, pos):
+        self.hovered = False
+        self.text = text
+        self.pos = pos
+        self.set_rect()
+        self.draw()
+
+    def draw(self):
+        self.set_rend()
+        screen.blit(self.rend, self.rect)
+
+    def set_rend(self):
+        myfont = pygame.font.Font(None, 60)
+        self.rend = myfont.render(self.text, True, self.get_color())
+
+    def get_color(self):
+        if self.hovered:
+            return (255, 255, 255)
+        else:
+            return (100, 100, 100)
+
+    def set_rect(self):
+        self.set_rend()
+        self.rect = self.rend.get_rect()
+        self.rect.topleft = self.pos
+###############################################
+def text_objects(text, font):
+    #writes instuctions on beginning page
+    paragraphSize = (470, 300)
+    fontSize = font.get_height()
+
+    paragraphSurface = pygame.Surface(paragraphSize ) 
+    paragraphSurface.fill((0, 255, 0))
+    paragraphSurface.set_colorkey((0, 255, 0))
+
+    splitLines = text.split(".")
+    offSet = (paragraphSize[1] - len(splitLines) * (fontSize + 1)) // 2 
+
+    for idx, line in enumerate(splitLines):
+        currentTextline = font.render(line, False, (255, 0, 0))
+        currentPostion = (0, idx * fontSize + offSet)
+        paragraphSurface.blit(currentTextline, currentPostion)
+
+    return paragraphSurface
+
+######
+num_hits= 0
+OPTIONS = 0
+BALLS= 1
+instructions = "Killing a pirate: 5 points.Killing a shark: 2 points.Killing a whale: 20 points."
+
 # initialize pygame
+pygame.mixer.init()
+pygame.mixer.pre_init(44100,-16,2, 3072)
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption(TITLE)
+#####Music Time#######
+pygame.mixer.music.load("stan.wav")
+pygame.mixer.music.set_volume(0.7)
+pygame.mixer.music.play()
+
 clock = pygame.time.Clock()
 
 # set up new game
 all_sprites = pygame.sprite.Group()
-mobs = pygame.sprite.Group()
+pirates = pygame.sprite.Group()
 bullets = pygame.sprite.Group()
-meteors= pygame.sprite.Group()
+sharks= pygame.sprite.Group()
+spears = pygame.sprite.Group()
+whales= pygame.sprite.Group()
 
-player = Player()
-all_sprites.add(player)
+captain = Captain()
+all_sprites.add(captain)
 
+for i in range(9):
+    p = Pirate()
+    all_sprites.add(p)
+    pirates.add(p)
 
-for i in range(8):
-    m = Mob()
-    all_sprites.add(m)
-    mobs.add(m)
+for i in range(6):
+    s= Shark()
+    all_sprites.add(s)
+    sharks.add(s)
 
-for i in range(8):
-    x= Meteor()
-    all_sprites.add(x)
-    meteors.add(x)
+for i in range (1):
+    w= Whale()
+    all_sprites.add(w)
+    whales.add(w)
+#############################
+options = [Option("NEW GAME", (100, 100))]
 
+STATE = OPTIONS
+mfont = pygame.font.Font(None, 40)
+x= text_objects(instructions, mfont)
 running = True
 while running:
-    clock.tick(FPS)
-    # check for events
-    for event in pygame.event.get():
-        # this one checks for the window being closed
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                player.shoot()
+    pygame.event.pump()
+    screen.fill((0, 0, 0))
 
-    ##### Game logic goes here  #########
-    all_sprites.update()
+    if STATE == OPTIONS:
+        screen.blit(x, (0, 200))
+        for option in options:
+            if option.rect.collidepoint(pygame.mouse.get_pos()):
+                option.hovered = True
+                if pygame.event.get([pygame.MOUSEBUTTONDOWN]) and option.text == "NEW GAME":
+                    STATE = BALLS
+            else:
+                option.hovered = False
+            option.draw()
+            pygame.display.update()
+    elif STATE == BALLS:
+        screen.fill(BGCOLOR)
+
+        clock.tick(FPS)
+        # check for events
+        for event in pygame.event.get():
+            # this one checks for the window being closed
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    captain.shoot()
+                elif event.key == pygame.K_z:
+                    captain.power()
+
+    ####RULES FOR GAME#######
+    
+        all_sprites.update()
     # check if bullets hit mobs
-    hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
-    for hit in hits:
-        m = Mob()
-        all_sprites.add(m)
-        mobs.add(m)
+        hits = pygame.sprite.groupcollide(pirates, bullets, True, True)
+        for hit in hits:
+            p = Pirate()
+            all_sprites.add(p)
+            pirates.add(p)
+            num_hits +=5
     #check if bullets hit meteor
-    hits= pygame.sprite.groupcollide(meteors, bullets, True, True)
-    for hit in hits:
-        x = Meteor()
-        all_sprites.add(x)
-        meteors.add(x)
+        hits= pygame.sprite.groupcollide(sharks, spears, True, True)
+        for hit in hits:
+            s= Shark()
+            all_sprites.add(s)
+            sharks.add(s)
+            num_hits += 2
+    # cheack if bullets hit whale
+        hits= pygame.sprite.groupcollide(whales, spears, True, True)
+        for hit in hits:
+            w= Whale()
+            all_sprites.add(w)
+            whales.add(w)
+            num_hits += 20
+            
+    # check if pirates hit captain
+        hits = pygame.sprite.spritecollide(captain, pirates, False, pygame.sprite.collide_circle)
+        if hits:
+            running = False
 
-    # check if mobs hit player
-    hits = pygame.sprite.spritecollide(player, mobs, False)
-    if hits:
-        running = False
-
+    #check if sharks hit captain
+        hits= pygame.sprite.spritecollide(captain, sharks, False, pygame.sprite.collide_circle)
+        if hits:
+            running = False
+    
     ##### Draw/update screen #########
-    screen.fill(BGCOLOR)
-    all_sprites.draw(screen)
+        myfont = pygame.font.Font(None, 30)
+        t=myfont.render("Hit Count: " + str(num_hits), False, (0, 255, 0))
+        screen.blit(t, (300, 570))
+    
+        all_sprites.draw(screen)
     # after drawing, flip the display
-    pygame.display.flip()
+        pygame.display.flip()
+print ('FINAL SCORE IS ' + str(num_hits))
